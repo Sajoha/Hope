c3a1d6039e5bfecfb68a60bb0a1df1bfdbd70f4d
SamJHaig <samjhaig@gmail.com>
2019-06-14 19:59:22 +0100

    [UI] Migrate All New Member Information into One Page

--- app/controllers/about.js
+++ app/controllers/about.js
@@ -18,22 +18,31 @@ function openVimeo() {
 	Ti.Platform.openURL('https://vimeo.com/user6896404');
 }
 
+function loadMap() {
+	Ti.Platform.openURL('http://maps.apple.com/?daddr=53.289818,-6.242318&dirflg=d');
+}
+
 $.tabbed.addEventListener('click', (e) => {
 	$.schedule.hide();
 	$.beliefs.hide();
+	$.location.hide();
 	$.socials.hide();
 
 	switch (e.index) {
-	case 0:
-		$.schedule.show();
-		break;
+		case 0:
+			$.schedule.show();
+			break;
 
-	case 1:
-		$.beliefs.show();
-		break;
+		case 1:
+			$.beliefs.show();
+			break;
 
-	case 2:
-		$.socials.show();
-		break;
+		case 2:
+			$.location.show();
+			break;
+
+		case 3:
+			$.socials.show();
+			break;
 	}
 });
